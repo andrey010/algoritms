e5915f0c33b08461c514bc0a082d58344e00db08
Andrii Bondarenko <andr780008@gmail.com>
2022-10-21 09:52:09 +0300

    delete files

--- 07-invert.js
+++
@@ -1,9 +0,0 @@
-// function invert(array) {
-//   array = array.map((num) => {
-//     if (num < 0) return Math.abs(num);
-//     return -num;
-//   });
-
-//   return array;
-// }
-const invert = array => array.map(num => -num);
